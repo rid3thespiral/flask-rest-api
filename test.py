0c6c65c764b351125623f917358e88f5d6788a65
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-31 13:01:09 +0200

    sqlalchemy, final commit

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-response = requests.put(BASE + "video/1", {"name":"nome1", "views":100, "likes":10})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1")
-print(response.json())
+response = requests.patch(BASE + "video/2", {"name": "ciao youtube", "likes":21})
+print(response)
+
+res=requests.get(BASE + "video/2")
+print(res.json())
